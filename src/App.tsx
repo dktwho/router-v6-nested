97ba42cccbd4ab21ed6d333f4430dd8d840638e3
Alexander Korobov <dktwho@mail.ru>
2023-07-24 16:17:00 +0300

    React router dom, practice nested route profile/settings - 2 method, used Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
 import React from 'react';
 
 import './App.css';
-import {NavLink, Route, Routes} from "react-router-dom";
+import {NavLink, Outlet, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-        <NavLink to={'/'}> main </NavLink>
-        <NavLink to={'/login'}> login </NavLink>
-        <NavLink to={'/profile'}> profile </NavLink>
-        <NavLink to={'/profile/settings'}> settings </NavLink>
+    return (
+        <div className="App">
+            <NavLink to={'/'}> main </NavLink>
+            <NavLink to={'/login'}> login </NavLink>
+            <NavLink to={'/profile'}> profile </NavLink>
+            <NavLink to={'/profile/settings'}> settings </NavLink>
 
-        <Routes>
-            <Route path={'/*'} element={<div>404</div>}/>
-            <Route path={'/'} element={<div>main</div>}/>
-            <Route path={'/login'} element={<div>login</div>}/>
-            <Route path={'/profile/*'} element={(
-                <div>
-                    profile
-                    <Routes>
-                        <Route path={'/settings'} element={<div>settings</div>}/>
-                    </Routes>
-                </div>
+            <Routes>
+                <Route path={'/*'} element={<div>404</div>}/>
+                <Route path={'/'} element={<div>main</div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+                {/*1- Method*/}
+                {/*<Route path={'/profile/*'} element={(*/}
+                {/*    <div>*/}
+                {/*        profile*/}
+                {/*        <Routes>*/}
+                {/*            <Route path={'/settings'} element={<div>settings</div>}/>*/}
+                {/*        </Routes>*/}
+                {/*    </div>*/}
 
-            )}/>
-        </Routes>
-    </div>
-  );
+                {/*)}/>*/}
+
+                {/*2 - Method*/}
+                <Route path={'/profile'} element={(
+                    <div>
+                        profile
+                        <Outlet/>
+                    </div>  )}>
+                    <Route path={'/profile/settings'} element={<div>settings</div>}/>
+              </Route>
+
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
